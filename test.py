145df8557fe5ce1046a02bab13fa7294de17cb2b
Trent Jones <trentjones21@gmail.com>
2015-09-22 15:34:23 -0600

    Now you can add a ring to the database

--- test.py
+++ test.py
@@ -33,16 +33,31 @@ def selectall():
     cur.close()
     conn.close()
     input("Found...")
+    mainMenu()
+
+def addRing():
+    conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='root', db='WilsonDiamonds')
+    cur = conn.cursor()
+    name = input("Ring name: ")
+    cur.execute("INSERT INTO inventory (name) VALUES ('%s')" % name)
+    cur.close()
+    conn.commit()
+    conn.close()
+    input("Added...")
+    mainMenu()
+
+
 
 def subMenu():
     clear()
     print("1) Print a diamond")
     print("2) Select All Rings")
+    print("3) Add A Ring")
     print("m) Main menu")
     selection = input("Select: ")
     valid = 1
     while (valid != 0):
-        if (selection == '1' or selection == '2' or selection == 'm'):
+        if (selection == '1' or selection == '2' or selection == '3' or selection == 'm'):
             valid = 0
         else:
             selection = input("Invalid selection. Select: ")
@@ -51,6 +66,9 @@ def subMenu():
         printDiamond()
     elif selection == '2':
         selectall()
+        valid = 0
+    elif selection == '3':
+        addRing()
         valid = 0
     elif selection == 'm':
         mainMenu()
