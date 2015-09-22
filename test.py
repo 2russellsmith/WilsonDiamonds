c28a6d1da5b52b8f5836db8fd3b56c3a98453129
Russell Smith <2russellsmith@gmail.com>
2015-09-22 15:11:44 -0600

    Testing Python

---
+++ test.py
@@ -0,0 +1,67 @@
+import os
+import pymysql
+
+clear = lambda: os.system('clear')
+
+clear()
+
+def mainMenu():
+    clear()
+    print("1) Print a diamond")
+    print("2) Sub Menu")
+    print("3) Quit")
+    selection = input("Select: ")
+    valid = 1
+    while valid != 0:
+        if selection == '1':
+            printDiamond()
+            valid = 0
+        elif selection == '2':
+            subMenu()
+            valid = 0
+        elif selection == '3':
+            valid = 0
+        else:
+            selection = input("Invalid selection. Select: ")
+
+def selectall():
+    conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', passwd='root', db='WilsonDiamonds')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM inventory")
+    for r in cur:
+        print(r)
+    cur.close()
+    conn.close()
+    input("Found...")
+
+def subMenu():
+    clear()
+    print("1) Print a diamond")
+    print("2) Select All Rings")
+    print("m) Main menu")
+    selection = input("Select: ")
+    valid = 1
+    while (valid != 0):
+        if (selection == '1' or selection == '2' or selection == 'm'):
+            valid = 0
+        else:
+            selection = input("Invalid selection. Select: ")
+
+    if (selection == '1'):
+        printDiamond()
+    elif selection == '2':
+        selectall()
+        valid = 0
+    elif selection == 'm':
+        mainMenu()
+        valid = 0
+
+def printDiamond():
+    clear()
+    f = open('diamond.txt', 'r')
+    diamond = f.read()
+    print(diamond)
+    input("Done...")
+    mainMenu()
+
+mainMenu()
